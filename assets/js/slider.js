99688547bb6e68ebef97bb9c3ad97d2f1a9a2328
Giorgi Matsukatovi <giorgimatsukatov@gmail.com>
2022-06-20 01:29:36 +0400

    dawhdwaud

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,15 +1,31 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
+var swiper = new Swiper(".howitworks-swiper", {
+    slidesPerView: 2,
+    spaceBetween: 24,
+    slidesPerGroup: 1,
+    loop: false,
     loopFillGroupWithBlank: true,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next1",
+        prevEl: ".swiper-button-prev1",
+    },
+});
+
+var swiper = new Swiper(".blog-posts-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 24,
+    slidesPerGroup: 1,
+    loop: false,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next1",
+        prevEl: ".swiper-button-prev1",
     },
 });
